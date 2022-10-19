1842868fafbfa05d15bcb13f692d0b1a89b9a172
ehsanul-haque <mdehaquesiam@gmail.com>
2022-10-20 00:37:59 +0600

    Prime 2

---
+++ PrimeNumber2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, number;
+    bool isPrime = true;
+    cout << "Enter a number";
+    cin >> number;
+    for (i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = false
+        }
+    }
+    if (isPrime == true)
+    {
+        cout << "Prime number";
+    }
+    else
+    {
+        cout << "Not a prime";
+    }
+
+    return 0;
+}
