f46e1ee859609e62605c172c889227646913717a
ehsanul-haque <mdehaquesiam@gmail.com>
2022-10-20 00:04:08 +0600

    fibonacci series

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, first = 0, second = 1, fibo = 0;
+    cout << "Enter a Range";
+    cin >> num;
+    for (; fibo <= num;)
+    {
+        cout << first << " ";
+        fibo = first + second;
+        first = second;
+        second = fibo;
+    }
+    return 0;
+}
