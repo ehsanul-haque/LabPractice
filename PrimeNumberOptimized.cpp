dbc56aee441c3503904e9c3a34ef8985696bdd17
ehsanul-haque <mdehaquesiam@gmail.com>
2022-10-20 00:24:59 +0600

    Prime Number

---
+++ PrimeNumberOptimized.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter a number : ";
+    cin >> number;
+    bool prime = true;
+    if (number <= 1)
+    {
+        prime = false;
+    }
+    for (int i = 2; i * i <= number; i++)
+    {
+        prime = true;
+    }
+    if (prime == true)
+    {
+        cout << number << " is a Prime Number" << endl;
+    }
+    else
+    {
+        cout << number << " is Not Prime Number" << endl;
+    }
+
+    return 0;
+}
