c50973ad65455e00d94177244fc3b33aeef334a4
Ehsanul Haque <114832270+ehsanul-haque@users.noreply.github.com>
2022-11-03 04:03:57 +0600

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -6,14 +6,14 @@ int main()
     int num, first = 0, second = 1, fibo = 0;
     cout << "Enter a Range";
     cin >> num;
-    for (; fibo <= num;)
+    for (; first <= num;)
     {
         cout << first << " ";
         fibo = first + second;
         first = second;
         second = fibo;
     }
-     /*  while (fibo <= num)
+     /*  while (first <= num)
     {
 
         cout << first << " ";
